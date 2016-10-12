9d9e92fc7a2ebf701f3fb5311ca23d0a33a76357
Austin-Dorff <austindorff@Austins-MacBook-Pro.local>
2016-10-12 15:37:50 -0500

    Add Base Files To Project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+
+  def index
+
+  end
+
+  def stackoverflow
+
+  end
+
+  def triplea
+
+  end
+
+  def relationaltones
+
+  end
+
+  def mechanica
+
+  end
+
+end
